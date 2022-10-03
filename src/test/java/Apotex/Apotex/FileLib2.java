c8fedf69f0a7cd0b421d1959b0aca9cafe68e6dc
Bharathi4 <bharathigbhavya@gmail.com>
2022-10-04 01:08:01 +0530

    Committing Filelib2 to Branchx

---
+++ src/test/java/Apotex/Apotex/FileLib2.java
@@ -0,0 +1,29 @@
+package Apotex.Apotex;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+
+/**
+ * This file is Generic class for data driven testing
+ * @author Bharathkumar
+ *
+ */
+public class FileLib2 {
+/**
+ * This is generic method to read the data from property file
+ * @param key
+ * @return
+ * @throws IOException
+ */
+
+public String getpropertydata(String key) throws IOException {
+	FileInputStream fis=new FileInputStream("./data/commontestdata.property.txt");
+	Properties p=new Properties();
+	p.load(fis);
+	String data = p.getProperty(key);
+	return data;
+  }
+}
